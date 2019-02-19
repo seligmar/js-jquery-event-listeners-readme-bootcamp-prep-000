45a83215de53f136991496dd1953345ef090f54a
Mary Selig <seligmar@gmail.com>
2019-02-19 19:06:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,23 +3,26 @@
 function getIt () {
   $('p').on("click", function () {
     alert("Hey!"); 
-     })} 
+  });
+} 
 
 
 
 function frameIt() {
   $('img').on('load', function() {
-  $('img').addClass('tasty');
-  })
+  $('img').addClass('tasty'); 
+  });
 }
 
 function pressIt () {
   $(document).on('keydown', function(key) { 
     if (key.which == 71) 
-  alert("G"); } ); 
+  alert("G"); 
+  }); 
 }
 
 function submitIt () {
   $("form").on("submit", function() {
-  alert("Your form is going to be submitted now.")}) 
+  alert("Your form is going to be submitted now.");
+  });  
 }
