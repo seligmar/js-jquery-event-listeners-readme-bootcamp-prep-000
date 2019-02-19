f3b5bfa4058b72c7bc4b6f160e7bc694fb837d83
Mary Selig <seligmar@gmail.com>
2019-02-19 19:07:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt () {
+  $('p').on("click", function () {
+    alert("Hey!"); 
+  });
+} 
 
-// call functions here
 
-});
+
+function frameIt() {
+  $('img').on('load', function() {
+  $('img').addClass('tasty'); 
+  });
+}
+
+function pressIt () {
+  $(document).on('keydown', function(key) { 
+    if (key.which == 71) 
+  alert("G"); 
+  }); 
+}
+
+function submitIt () {
+  $("form").on("submit", function() {
+  alert("Your form is going to be submitted now.");
+  });  
+}
